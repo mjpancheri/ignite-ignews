134a4d66b4a11c191abfa83e756e658cac09f636
Marcio Pancheri <mjpancheri@claretiano.edu.br>
2021-03-24 22:31:41 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,61 @@
+// import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { stripe } from '../services/stripe';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
+
+// export const getServerSideProps: GetServerSideProps = async () => { // SSR - Server Side Rendering
+export const getStaticProps: GetStaticProps = async () => { // SSG Static Site Generation
+  const price = await stripe.prices.retrieve('price_1IYTlgIVm3hxfq6e3Shmfk2M', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+}
