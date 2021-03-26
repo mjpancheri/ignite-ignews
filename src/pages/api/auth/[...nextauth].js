6734bbbf66a4a97b2e58fc64b175574fccf7ea10
Marcio Pancheri <mjpancheri@claretiano.edu.br>
2021-03-26 00:24:39 -0300

    Config FaunaDB and Stripe

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,68 @@
+import { query as q } from 'faunadb';
+
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      scope: 'read:user'
+    }),
+    // ...add more providers here
+  ],
+  /*jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  
+    // You can also specify a public key for verification if using public/private key (but private only is fine)
+    // verificationKey: process.env.JWT_SIGNING_PUBLIC_KEY,
+  
+    // If you want to use some key format other than HS512 you can specify custom options to use
+    // when verifying (note: verificationOptions should include a value for maxTokenAge as well).
+    // verificationOptions = {
+    //   maxTokenAge: `${maxAge}s`, // e.g. `${30 * 24 * 60 * 60}s` = 30 days
+    //   algorithms: ['HS512']
+    // },
+  },*/
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+  }
+  // A database is optional, but required to persist accounts in a database
+  // database: process.env.DATABASE_URL,
+})
